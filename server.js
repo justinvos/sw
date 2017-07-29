412f526efb475b38d766b9aae8cf7eef0e78725d
justinvos <justinvosnz@gmail.com>
2017-07-29 23:56:16 +1200

    Added static webpage serving

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Server loaded")
+var express = require('express')
+var path = require('path')
+
+var app = express()
+
+const PORT = process.env.PORT || 3001
+
+console.log('Server loaded')
+
+app.use(express.static(path.join(__dirname, './public')))
+app.listen(PORT, function () {
+  console.log('Listening on port', PORT)
+})
