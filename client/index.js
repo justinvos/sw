173fb64ca3274108b925fd38fdf7f3e845f02a36
justinvos <justinvosnz@gmail.com>
2017-07-30 01:07:59 +1200

    Added notifications and refactored the scripts into the bundle

--- client/index.js
+++ client/index.js
@@ -3,11 +3,35 @@ import ReactDOM from 'react-dom'
 
 import App from './components/App'
 
+function notify(title, body) {
+  Notification.requestPermission(function(result) {
+    if (result === 'granted') {
+      navigator.serviceWorker.ready.then(function(registration) {
+        registration.showNotification(title, {
+          body: body,
+          icon: '',
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
+          tag: ''
+        });
+      });
+    }
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <App />,
     document.getElementById('app')
   )
 })
+
+if("serviceWorker" in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function(reg) {
+    console.log('Registraion succeeded')
+    notify("Notification Title", "Test body")
+  }).catch(function(error) {
+    console.log('Registration failed with ' + error);
+  })
+}
 
 console.log("index.js loaded")
--- public/main.js
+++
@@ -1,7 +0,0 @@
-if("serviceWorker" in navigator) {
-  navigator.serviceWorker.register('sw.js').then(function(reg) {
-    console.log('Registraion succeeded')
-  }).catch(function(error) {
-    console.log('Registration failed with ' + error);
-  })
-}
